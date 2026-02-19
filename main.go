28746fa54ec7ac8905a458585fb2c15058b4746b
Trey Copeland <jcopetre@gmail.com>
2026-02-18 21:12:43 -0600

    wraps core logic in run(), letting main() be for CLI

--- main.go
+++ main.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/trey.copeland/bootdev_blogag/internal/config"
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return fmt.Errorf("Read config: %w", err)
 	}
 
 	if err := cfg.SetUser("trey"); err != nil {
-		fmt.Println(err)
-		return
+		return fmt.Errorf("Set user: %w", err)
 	}
 
 	cfg, err = config.Read()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return fmt.Errorf("Read: %w", err)
 	}
+
 	fmt.Println(cfg)
+	return nil
 }
