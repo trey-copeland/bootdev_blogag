2274434e81f14affdba1f3b25c5ad2d275fa4ee3
Trey Copeland <jcopetre@gmail.com>
2026-02-19 22:15:55 -0600

    adds postgres interaction for login and register

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/trey.copeland/bootdev_blogag
 
 go 1.25.5
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/trey.copeland/bootdev_blogag/internal/config"
+	"github.com/trey.copeland/bootdev_blogag/internal/database"
 )
 
 type command struct {
@@ -13,7 +18,8 @@ type command struct {
 }
 
 type state struct {
-	config *config.Config
+	config  *config.Config
+	queries *database.Queries
 }
 
 type commands struct {
@@ -41,9 +47,46 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("Too many arguments provided to login")
 	}
 
+	name := cmd.args[0]
+	_, err := s.queries.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("User doesn't exist")
+	}
 	s.config.SetUser(cmd.args[0])
 
 	fmt.Println("User has been set")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("No argument provided to register")
+	}
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Too many arguments provided to register")
+	}
+
+	name := cmd.args[0]
+	_, err := s.queries.GetUser(context.Background(), name)
+	if err == nil {
+		return fmt.Errorf("User already exists")
+	}
+
+	s.config.SetUser(name)
+
+	now := time.Now()
+	userParams := database.CreateUserParams{
+		Name:      name,
+		CreatedAt: now,
+		UpdatedAt: now,
+		ID:        uuid.New(),
+	}
+	dbUser, err := s.queries.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return fmt.Errorf("Error creating user: %w", err)
+	}
+	fmt.Printf("User created: %v", dbUser)
+
 	return nil
 }
 
@@ -75,8 +118,15 @@ func run(cmd command) error {
 		return fmt.Errorf("Read config: %w", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		return fmt.Errorf("Database access error: %w", err)
+	}
+	dbQueries := database.New(db)
+
 	appState := state{
-		config: &cfg,
+		config:  &cfg,
+		queries: dbQueries,
 	}
 
 	cmdMap := make(map[string]func(*state, command) error)
@@ -84,6 +134,7 @@ func run(cmd command) error {
 		cmdMap: cmdMap,
 	}
 	appCmds.register("login", handlerLogin)
+	appCmds.register("register", handlerRegister)
 
 	if err := appCmds.run(&appState, cmd); err != nil {
 		return err
